c16121bc24b56e789abb3e7e61193670d3f14c04
Mohd Sufiyan Ansari <addsufiyan@gmail.com>
2020-11-01 11:39:17 +0530

    Changed Readme

--- backend/VirtualHealthExamination/VirtualHealthExamination/settings.py
+++ backend/VirtualHealthExamination/VirtualHealthExamination/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Email Setting
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = '' #enter your email here
-EMAIL_HOST_PASSWORD = '' #enter your password here
+EMAIL_HOST_USER = 'nozariyo@gmail.com' #enter your email here 
+EMAIL_HOST_PASSWORD = 'AbCd1303' # enter your password here
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
